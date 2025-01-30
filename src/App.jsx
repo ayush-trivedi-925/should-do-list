3e60c8afb6b37a2bef7e264ed2c4c6845b8cc83b
ayush-trivedi-925 <asxu525@gmail.com>
2025-01-30 11:02:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Tabs from "./components/Tabs";
+import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
+import "./index.css";
+import "./fanta.css";
+function App() {
+  return (
+    <>
+      <Header />
+      <Tabs />
+      <TodoList />
+      <TodoInput />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1 className="text-gradient">You have # open tasks.</h1>
+      </header>
+    </>
+  );
+}
