81853e4eab8860b2e45d556bf1dbdf85d539474a
ayush-trivedi-925 <asxu525@gmail.com>
2025-01-30 12:28:47 +0530

    Added logic to add a todo

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,29 @@ import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
 import "./index.css";
 import "./fanta.css";
+import { useState } from "react";
 function App() {
-  const todos = [
+  // const todos = [
+  //   { input: "Wake up early in the morning", complete: false },
+  //   { input: "Eat healthy or in limit", complete: true },
+  //   { input: "Try to follow the routine", complete: true },
+  //   { input: "Try to give my best everyday", complete: false },
+  // ];
+  const [todos, setTodos] = useState([
     { input: "Wake up early in the morning", complete: false },
-    { input: "Eat healthy or in limit", complete: true },
-    { input: "Try to follow the routine", complete: true },
-    { input: "Try to give my best everyday", complete: false },
-  ];
+  ]);
+
+  const handleAddTodo = (newTodo) => {
+    const newTodoList = [...todos, { input: newTodo, complete: false }];
+    setTodos(newTodoList);
+  };
+
   return (
     <>
       <Header todos={todos} />
       <Tabs todos={todos} />
       <TodoList todos={todos} />
-      <TodoInput />
+      <TodoInput handleAddTodo={handleAddTodo} />
     </>
   );
 }
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,3 +1,26 @@
-export default function TodoInput() {
-  return <div></div>;
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+export default function TodoInput({ handleAddTodo }) {
+  const [inputValue, setInputValue] = useState("");
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (!inputValue) {
+            return;
+          }
+          handleAddTodo(inputValue);
+          setInputValue("");
+        }}
+      >
+        <FaPlus />
+      </button>
+    </div>
+  );
 }
